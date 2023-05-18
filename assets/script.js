37f5e9c42e524e3f2f98b20b679029ca8d173444
Calvin Roberts <you@example.com>
2023-05-18 09:21:18 -0600

    added JS for MP

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,48 @@
-var items = ["R", "P", "S"];
+//DATA =======================
+var options = "RPS".split(""); // ["R", "P", "S"]
 
 
-var playGame = true;
+// HELPER FUNCTIONS ========================
+function getUserChoice (){
+    //prompt user to enter one fo the options (R, P, S)
+    var userChoice = prompt("Please enter R, P, or S").toUpperCase();
+    if (!options.includes(userChoice)) {
+        return getUserChoice();
+    }
+    //return the choice
+    return userChoice;
+}
+
+function getComputerChoice() {
+    var randomSeed = Math.random();
+    var scale = options.length;
+
+    var randomIndex = randomSeed * scale;
+    console.log(randomSeed)
+    var choice = options[randomIndex];
+    return choice;
+}
+
+// function tie (computerChoice) {
+//     ties++
+//     alert("Computer chose" + computerChoice + ".")
+//     alert("It's a tie!")
+// }
+// function win
+// function loss
+// function checkWin
+
+function playGame (){
+    console.log("The game is starting...")
+    // get the user's choice
+    var userChoice = getUserChoice();
+    console.log(userChoice);
+    // computer makes a choice
+    var computerChoice = getComputerChoice();
+    console.log("Computer choice: ", computerChoice);
+    // display scores
+    // ask if they want to play again
+}
+
+// INITITALIZATIONS ========
+playGame();
